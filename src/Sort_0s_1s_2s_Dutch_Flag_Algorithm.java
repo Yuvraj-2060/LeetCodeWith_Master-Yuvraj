200f9c2e058ffaff2ee49b7272ecc436004cfcc3
Yuvraj-2060 <yuvrajthakur2060@gmail.com>
2023-12-20 23:38:10 +0530

    Added 1 comment for Time Space Complexity Analysis.

--- src/Sort_0s_1s_2s_Dutch_Flag_Algorithm.java
+++ src/Sort_0s_1s_2s_Dutch_Flag_Algorithm.java
@@ -1,5 +1,9 @@
-import java.util.Arrays;
+//Time Complexity: O(N), where N = size of the given array.
+//        Reason: We are using a single loop that can run at most N times.
 
+//        Space Complexity: O(1) as we are not using any extra space.
+
+import java.util.Arrays;
 public class Sort_0s_1s_2s_Dutch_Flag_Algorithm {
 
     public static void sortColors(int[] nums) {
