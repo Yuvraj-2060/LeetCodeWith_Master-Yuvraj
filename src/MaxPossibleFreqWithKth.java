47a5cb9567ca19566ee5295cd9a2f4f19cbd734d
Yuvraj-2060 <yuvrajthakur2060@gmail.com>
2024-01-25 01:09:54 +0530

    LeetCode Problem:1838. Max frequency with Kth operation allowed.

---
+++ src/MaxPossibleFreqWithKth.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class MaxPossibleFreqWithKth {
+    public static int maxFrequency(int[] nums, int k) {
+        int maxFrequency = 0;
+        long currentSum = 0;
+
+        Arrays.sort(nums); // Sort the array in ascending order
+
+        for (int left = 0, right = 0; right < nums.length; ++right) {
+            currentSum += nums[right];
+
+            while (currentSum + k < (long) nums[right] * (right - left + 1)) {
+                currentSum -= nums[left];
+                left++;
+            }
+
+            maxFrequency = Math.max(maxFrequency, right - left + 1);
+        }
+
+        return maxFrequency;
+    }
+    public static void main(String[] args) {
+        int []nums = {1,2,4};
+        int k = 5;
+        System.out.println(maxFrequency(nums,k));
+    }
+}
+
