8169f96e356810edd30c50bb18a3d97884a83109
Yuvraj-2060 <yuvrajthakur2060@gmail.com>
2024-01-30 20:45:59 +0530

    Non_Repeating Element In Array: I'm commiting 2 approach to find Non Repeating Element. (1) XOR & (2) HashSet

---
+++ src/NonRepeatedElement1.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class NonRepeatedElement1 {
+    public static int getSingleElement1(int []arr){
+        int xorr = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            xorr = xorr ^ arr[i];
+        }
+        return (int)xorr;
+    }
+    public static void main(String[] args) {
+        int nums[] = {8,8,9,9,10};
+        int k = 7;
+
+        System.out.println(" "+ Arrays.toString(nums));
+        System.out.println("Non Repeated Element Is: "+getSingleElement1(nums));
+
+    }
+}
