b82c0b73a65c38acaeb278e34c76358fc70378a8
Yuvraj-2060 <yuvrajthakur2060@gmail.com>
2024-02-08 23:42:42 +0530

    LeetCode Problem No: 560 (Medium) Done using by 2 Approach [1. For Loops | 2. HashMap]

---
+++ src/Count_Of_Kth_SubArray_Sum1.java
@@ -0,0 +1,22 @@
+public class Count_Of_Kth_SubArray_Sum1 {
+
+    public static int subarraySum(int[] nums, int k) {
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            int sum=0;
+            for(int j=i;j<nums.length;j++){
+                sum=sum+nums[j];
+                if(sum==k){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,2,3};
+        int k=3;
+
+        System.out.println("Count of subarray sum equals to: "+k+" is "+subarraySum(arr,k));
+    }
+}
