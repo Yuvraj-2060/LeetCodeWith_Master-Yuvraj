dceae7cda81bcd8dbcb69a128a53290bd1d031b6
Yuvraj-2060 <yuvrajthakur2060@gmail.com>
2024-02-09 00:35:41 +0530

    Coding Ninjas Problem: Find a list of all superior elements in an array.

---
+++ src/SuperiorElement1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class SuperiorElement1 {
+    public static List< Integer > superiorElements(int []a) {
+        List<Integer> ans = new ArrayList<>();
+
+        int n = a.length;
+        int max = Integer.MIN_VALUE;
+
+        for(int i=n-1; i>=0;i--){
+            if(a[i]>max){
+                ans.add(a[i]);
+                max = a[i];
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int arr[] ={1, 2, 2, 1};
+        List ls = superiorElements(arr);
+        System.out.println("Superior Elements are: "+ls);
+    }
+}
