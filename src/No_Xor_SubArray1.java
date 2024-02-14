e5e6ac87d7c1e4a81eb4ddf161b99ac5fcfd480e
Yuvraj-2060 <yuvrajthakur2060@gmail.com>
2024-02-14 21:51:08 +0530

    Coding Ninjas Problem: Subarrays with XOR ‘K’

--- src/No_Xor_SubArray1.java
+++ src/No_Xor_SubArray1.java
@@ -27,3 +27,23 @@ public class No_Xor_SubArray1 {
         return c;
     }
 }
+
+//                This Logic Also Works: Slight change in the HashMap Logic.
+
+
+//    public static int subarraysWithSumK(int []a, int b) {
+//        // Write your code here
+//        Map<Integer, Integer> freqMap = new HashMap<>();
+//        int count = 0;
+//        int xor = 0;
+//        for (int i = 0; i < a.length; i++) {
+//            xor ^= a[i];
+//            if (xor == b)
+//                count++;
+//            if (freqMap.containsKey(xor ^ b)) {
+//                count += freqMap.get(xor ^ b);
+//            }
+//            freqMap.put(xor, freqMap.getOrDefault(xor, 0) + 1);
+//        }
+//        return count;
+//    }
