c10957b68ebf0bbe08121ce713a2530eb2a0d3a3
Yuvraj-2060 <yuvrajthakur2060@gmail.com>
2024-02-28 13:29:13 +0530

    Coding Ninjas Problem: Find the Ceil & Floor of target value. Solved using BINARY SEARCH ALGO*. By finding upper & lower bound.

---
+++ src/Find_Lower_Upper_Bound.java
@@ -0,0 +1,52 @@
+public class Find_Lower_Upper_Bound {
+    public static int[] getFloorAndCeil(int[] a, int n, int x) {
+
+        int f = findFloor(a,n,x);
+        int c = findCeil(a,n,x);
+
+        return new int[]{f,c};
+
+    }
+    static int findFloor(int[] arr, int n, int x) {
+        int low = 0, high = n - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low+ (high-low) / 2;
+
+            if (arr[mid] <= x) {
+                ans = arr[mid];
+                //look for smaller index on the left
+                low = mid + 1;
+            } else {
+                high = mid - 1; // look on the right
+            }
+        }
+        return ans;
+    }
+
+    static int findCeil(int[] arr, int n, int x) {
+        int low = 0, high = n - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low+ (high-low) / 2;
+            // maybe an answer
+            if (arr[mid] >= x) {
+                ans = arr[mid];
+                //look for smaller index on the left
+                high = mid - 1;
+            } else {
+                low = mid + 1; // look on the right
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 4, 4, 7, 8, 10};
+        int n = 6, x = 5;
+        int[] ans = getFloorAndCeil(arr, n, x);
+        System.out.println("The floor and ceil are: " + ans[0]+ " " + ans[1]);
+    }
+}
